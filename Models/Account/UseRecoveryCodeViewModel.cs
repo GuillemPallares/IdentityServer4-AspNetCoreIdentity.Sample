a901c3ab16e860117c713a86da2c642a8a9fdaed
Guillem <guillempallares@outlook.es>
2021-04-18 21:33:46 +0200

    feat(Login): Added Two Factor Authentication.

---
+++ Models/Account/UseRecoveryCodeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityServerHost.Models.Account
+{
+    public class UseRecoveryCodeViewModel
+    {
+        [Required]
+        public string Code { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
