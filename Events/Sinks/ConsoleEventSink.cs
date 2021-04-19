8cbbc24c33f921cf084c68ca930c8fcef16efcf6
Guillem <guillempallares@outlook.es>
2021-04-19 20:05:30 +0200

    refactor(EventSinks): Created ConsoleEventSinkClass.

---
+++ Events/Sinks/ConsoleEventSink.cs
@@ -0,0 +1,49 @@
+using System.Threading.Tasks;
+using IdentityServer4.Events;
+using IdentityServer4.Services;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Themes;
+
+namespace IdentityServerHost.Events.Sinks
+{
+    class ConsoleEventSink : IEventSink
+    {
+        public readonly Logger _log;
+
+        public ConsoleEventSink()
+        {
+            _log = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
+                .MinimumLevel.Override("System", LogEventLevel.Warning)
+                .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Code)
+                .CreateLogger();
+        }
+
+        public Task PersistAsync(Event evt)
+        {
+            if (evt.EventType == EventTypes.Success ||
+                evt.EventType == EventTypes.Information)
+            {
+                _log.Information("{Name} ({Id}), Details: {@details}",
+                    evt.Name,
+                    evt.Id,
+                    evt);
+            }
+            else
+            {
+                _log.Error("{Name} ({Id}), Details: {@details}",
+                    evt.Name,
+                    evt.Id,
+                    evt);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
